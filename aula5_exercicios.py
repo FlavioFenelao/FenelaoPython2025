68a3fcee904c858abb95b3b4b50007b592eea254
FlavioFenelao <ffenelao@gmail.com>
2025-03-10 21:45:53 -0300

    Add files via upload

---
+++ aula5_exercicios.py
@@ -0,0 +1,38 @@
+preco = 150
+desconto = 20
+preco_desconto = preco - desconto 
+print(preco_desconto)
+
+peso = 70
+altura = 1.75
+imc = peso /(altura **2)
+print(imc)
+
+idade = 22
+tempo_experiencia = True
+
+Elegível = idade >= 18 and tempo_experiencia
+print("Elegível")
+
+nivel_acesso = int(input("Digite o seu nível de acesso: "))
+
+if nivel_acesso == 3:
+    print("Acesso total")
+elif nivel_acesso == 2:
+    print("Acesso parcial")
+elif nivel_acesso == 1:
+    print("Acesso restrito")
+else:
+    print("Digite o código válido!!!")
+
+
+celsius = int(input("Digite uma temperatura: "))
+fahrenheit = (celsius * 9/5) + 32
+print(fahrenheit)
+
+valor = int(input("Digite um valor: "))
+if valor %2 == 0:
+    print("O número é par")
+else:
+    print("O número é ímpar")
+print("valor")
