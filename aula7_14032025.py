70adcc4dd16bdcfdc9b3e8c5f30468111e2ed111
FlavioFenelao <ffenelao@gmail.com>
2025-03-16 17:21:44 -0300

    Update aula7_14032025.py

--- aula7_14032025.py
+++ aula7_14032025.py
@@ -24,25 +24,10 @@ for i in range(numero, -1, -1):
     for i in range(1, 11):
         print(f"{numero} x {i} = {numero*i}")
 
- #  4.Cálculo de Média de Notas: Peça ao usuário que insira 4 notas (de 0 a 10). 
-Calcule a média das notas e exiba o resultado. 
-Se a média for maior ou igual a 7, exiba "Aprovado". Caso contrário, exiba "Reprovado".
-nota1 = float(input("Insira a primeira nota (0 a 10): "))
-nota2 = float(input("Insira a segunda nota (0 a 10): "))
-nota3 = float(input("Insira a terceira nota (0 a 10): "))
-nota4 = float(input("Insira a quarta nota (0 a 10): "))
-media = (nota1 + nota2 + nota3 + nota4) / 4
-print(f"A média das notas é: {media:.2f}")
-if media >= 7: 
-    print("Aprovado") 
-else:
-    print("Reprovado")
-     
-# 5. dos Números Pares em um Intervalo: Peça ao usuário dois números, representando o início e o fim de um intervalo. Use um loop (for ou while) para calcular a soma de todos os números pares nesse intervalo e exiba o resultado.
-inicio = int(input("Insira o número inicial do intervalo: "))
-fim = int(input("Insira o número final do intervalo: "))
-soma_pares = 0
-for numero in range(inicio, fim + 1):
-    if numero % 2 == 0: 
-      soma_pares += numero
-print(f"A soma dos números pares no intervalo de {inicio} a {fim} é: {soma_pares}")
+# 4. Números Pares com FOR:
+# Peça ao usuário um número e mostre todos os números pares de 1 até ele.
+numero = int(input("Insira um número: "))
+print(f"Números pares de 1 até {numero}:")
+for i in range(1, numero + 1):   
+    if i % 2 == 0:  # Verificar se o número é par 
+        print(i)
