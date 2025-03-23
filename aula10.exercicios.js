0acb79dfcace474039002306c01f13551ea662a2
FlavioFenelao <ffenelao@gmail.com>
2025-03-23 20:24:14 -0300

    Add files via upload

--- aula10.exercicios.js
+++ aula10.exercicios.js
@@ -2,11 +2,105 @@
 
 //1.Soma de Matrizes: Crie duas matrizes 3x3 e escreva uma função para somá-las. 
 //A função deve retornar uma nova matriz com o resultado.
-
+// Definir duas matrizes 3x3
+let matriz1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+  
+  let matriz2 = [
+    [9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1]
+  ];
+  
+  // Função para somar as matrizes
+  function somarMatrizes(matrizA, matrizB) {
+    let resultado = [];
+  
+    for (let i = 0; i < matrizA.length; i++) {
+      let linha = [];
+      for (let j = 0; j < matrizA[i].length; j++) {
+        linha.push(matrizA[i][j] + matrizB[i][j]);
+      }
+      resultado.push(linha);
+    }
+  
+    return resultado;
+  }
+  
+  // Chamar a função e armazenar o resultado
+  let resultadoSoma = somarMatrizes(matriz1, matriz2);
+  
+  // Exibir o resultado
+  console.log("Resultado da soma das matrizes:");
+  console.log(resultadoSoma);
+  
 //2.Transposição de Matriz: Escreva uma função que receba uma matriz 3x3 e retorne sua transposta (troque linhas por colunas).
-
+function transporMatriz(matriz) {
+    let matrizTransposta = [];
+    
+    for (let i = 0; i < matriz[0].length; i++) {
+      let novaLinha = [];
+      for (let j = 0; j < matriz.length; j++) {
+        novaLinha.push(matriz[j][i]);
+      }
+      matrizTransposta.push(novaLinha);
+    }
+  
+    return matrizTransposta;
+  }
+  
+  // Exemplo de uso
+  const matriz3x3 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+  
+  const matrizTransposta = transporMatriz(matriz3x3);
+  console.log(matrizTransposta);
+  
 //3.Multiplicação de Matrizes: Crie duas matrizes 2x2 e escreva uma função para multiplicá-las.
-
+// Função para multiplicar duas matrizes 2x2
+function multiplicarMatrizes(matrizA, matrizB) {
+    // Inicializando a matriz resultado com 2x2, preenchida com 0s
+    let resultado = [
+      [0, 0],
+      [0, 0]
+    ];
+  
+    // Multiplicando as matrizes
+    for (let i = 0; i < 2; i++) {
+      for (let j = 0; j < 2; j++) {
+        for (let k = 0; k < 2; k++) {
+          resultado[i][j] += matrizA[i][k] * matrizB[k][j];
+        }
+      }
+    }
+  
+    return resultado;
+  }
+  
+  // Definindo duas matrizes 2x2
+  let matrizA = [
+    [1, 2],
+    [3, 4]
+  ];
+  
+  let matrizB = [
+    [5, 6],
+    [7, 8]
+  ];
+  
+  // Chamando a função de multiplicação
+  let resultado = multiplicarMatrizes(matrizA, matrizB);
+  
+  // Exibindo o resultado
+  console.log("Resultado da multiplicação das matrizes:");
+  console.log(resultado);
+  
 //4.Jogo da Velha: Implemente um jogo da velha usando uma matriz 3x3.
 // O programa deve permitir que dois jogadores façam jogadas alternadas e verifique se há um vencedor.
 
